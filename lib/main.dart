64832788567f0a4256e99626c1d53fdb58f024e7
matheus f <math.fmartins42@gmail.com>
2021-02-23 18:09:52 -0300

    layout parte 1

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
       return MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.blue
+        ),
         home: Scaffold(
           appBar: AppBar(
             title: Text('IMC'),
             centerTitle: false,
           ),
-          body: Text('Hello World')
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+             Flexible(
+               flex: 1,
+               child: Container(
+                 margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                 child: Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                   children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Peso',
+                        hintText: 'Peso em kg.'
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                    Container( // ou SizedBox
+                      height: 10
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Altura',
+                        hintText: 'Altura em metros.'
+                      ),
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                        signed: false,
+                      ),
+                    ),
+                     Container( //ou SizedBox
+                      height: 10
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColor,
+                        child: Text(
+                          'Calcular IMC',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16
+                          ),
+                        ),
+                        onPressed: () {
+                          //função
+                        },
+                      ),
+                    ),
+                 ],),
+               )
+             ),
+             Flexible(
+               flex: 2,
+               child: Center(child: Image.asset('assets/images/abaixo.png')),
+             )
+            ],
+          )
         )
       );
   }
